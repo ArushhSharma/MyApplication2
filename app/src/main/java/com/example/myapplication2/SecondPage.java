f21198a10baeced2169f2e73c7bd005254a1c700
Arush Sharma <arush.sharma@moengage.com>
2023-05-14 23:46:45 +0530

    Changed the button position, added logout

--- app/src/main/java/com/example/myapplication2/SecondPage.java
+++ app/src/main/java/com/example/myapplication2/SecondPage.java
@@ -2,7 +2,12 @@ package com.example.myapplication2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 public class SecondPage extends AppCompatActivity {
 
@@ -10,5 +15,17 @@ public class SecondPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_page);
-    }
+
+        MaterialButton logout = (MaterialButton) findViewById(R.id.logoutbtn);
+
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    Intent intent = new Intent(SecondPage.this,MainActivity.class);
+                    startActivity(intent);
+
+                };
+            });
+    };
 }
